1cf3c829b20d5d16cad37be587d415492ff2dbad
Andriy Strizhak <115723613+andriystrizhak@users.noreply.github.com>
2023-09-15 20:01:32 +0300

    Revert "Feature/ef sqlite integration"

--- EWL_Tests/SQL-Requests/Вивести певну кількість слів певної категорії відсортованих за часом додавання і за рейтингом.sql
+++
@@ -1,7 +0,0 @@
-SELECT AllWords.WordID, AllWords.EngWord, AllWords.UaTranslation, AllWords.Rating, AllWords.Repetition, WordCategories.AddedAt
-FROM AllWords
-JOIN WordCategories
-ON AllWords.WordID = WordCategories.WordID
-WHERE WordCategories.CategoryID = 1
-ORDER BY AllWords.Rating, WordCategories.AddedAt
-LIMIT 2
--- EWL_Tests/SQL-Requests/Видалення всіх елементів окрім перших двох.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM Categories 
-WHERE CategoryID NOT IN ( 
-	SELECT CategoryID 
-	FROM Categories 
-	ORDER BY CategoryID 
-	LIMIT 2
-);
